623f141b78883b323ed118bcecc84aa02073b554
Privalov Artem <rashen95@mail.ru>
2026-06-20 19:44:52 +0300

    Добавление модулей и изменение структуры проекта

---
+++ ms-1/src/test/java/ru/privalov/messagingservice/ms1/Ms1ApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.privalov.messagingservice.ms1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
