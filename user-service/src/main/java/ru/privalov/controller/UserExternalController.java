77c9b3f95fcea08d089c39761c7fc4378947a608
Privalov Artem <rashen95@mail.ru>
2026-06-28 13:53:17 +0300

    Доработки

--- user-service/src/main/java/ru/privalov/controller/UserExternalController.java
+++ user-service/src/main/java/ru/privalov/controller/UserExternalController.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
